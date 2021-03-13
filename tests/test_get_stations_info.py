e8ae44cb75cd12ad064e25021e228b2ef85d2481
chirag <chiiragrank@gmail.com>
2021-03-13 10:54:30 -0500

    Fix the test and code of stations info

--- tests/test_get_stations_info.py
+++ tests/test_get_stations_info.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pytest
 from noaastn import noaastn
 from pandas.api.types import is_datetime64_any_dtype as is_datetime
@@ -5,6 +6,16 @@ from pandas.api.types import is_datetime64_any_dtype as is_datetime
 get_stations_info_params = [("all", [29561, 253]), ("US", [7158, 1])]
 invalid_params = ["XXX", 12]
 num_column = 11
+
+col_len = {
+    "country": 2,
+    "state": 2,
+    "usaf": 6,
+    "wban": 5,
+    "latitude": 7,
+    "longitude": 8,
+    "elevation": 7,
+}
 
 
 @pytest.mark.parametrize("param, expected_output", get_stations_info_params)
@@ -21,13 +32,27 @@ def test_get_stations_info(param, expected_output):
     is_datetime(data_all["start"])
     is_datetime(data_all["end"])
 
-    # check the number of rows and the number of countries in dataframe
-    assert data_all.shape[0] == expected_output[0]
-    assert len(set(data_all["country"])) == expected_output[1]
+    # match and check each col value pattern by comparing length
+    row_df = data_all.sample(1)
+    for col in col_len.keys():
+        assert (
+            pd.isna(row_df[col].values[0])
+            or pd.isnull(row_df[col].values[0])
+            or len(row_df[col].values[0]) == col_len[col]
+        )
 
 
 def test_get_stations_info_exception():
-    for param in invalid_params:
-        with pytest.raises(Exception) as ex_info:
-            noaastn.get_stations_info(country=param)
-            assert str(ex_info.value) == "Invalid country parameter"
+    with pytest.raises(Exception) as ex_info:
+        assert noaastn.get_stations_info(country=12)
+    assert (
+        str(ex_info.value)
+        == "Invalid country parameter. parameter should be a string"
+    )
+
+    with pytest.raises(Exception) as ex_info:
+        assert noaastn.get_stations_info(country="XXX")
+    assert (
+        str(ex_info.value)
+        == "Invalid country parameter. parameter should be length 2"
+    )
